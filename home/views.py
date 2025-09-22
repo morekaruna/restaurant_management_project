eac2864eba45d1daf53d4be31290e435ce1271f0
morekaruna <karunakaruna2711@gmail.com>
2025-09-22 23:31:32 +0530

    43771

--- home/views.py
+++ home/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     context = {
-        "restaurant_name": settings.RESTAURANT_NAME
+        "restaurant_name": getattr(settings, "RESTAURANT_NAME", "My Restaurant"),
     }
     return render(request, "home.html", context)
+
+def about_view(request):
+    context = {
+        "restaurant_name": getattr(settings, "RESTAURANT_NAME", "My Restaurant"),
+        "description" : "We serve the best dishes with fresh ingredients, warm hospitality, and a cozy atmosphere."
+    }
+    return render(request, "about.html", context)
